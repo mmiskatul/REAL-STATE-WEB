68a2e9949709e94b0e0d2827f974e004ffccea56
mmiskatul <masabimiskat@gmail.com>
2025-05-06 09:17:03 +0600

    initial setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-// import { useState } from 'react'
-import './App.css'
+import React from 'react'
+import { DarkModeProvider } from './component/DarkModeContext'
+import Header from './component/Header'
+import Footer from './component/Footer'
+import Hero from './sections/Hero'
+import About from './sections/About'
+import PopularAreas from './sections/PopularAreas'
+import Properties from './sections/Properties'
+import Services from './sections/Services'
+import Clients from './sections/Clients'
+import Contact from './sections/Contact'
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
-     <h1 className=''></h1>
-        
+    <DarkModeProvider>
+    <Header/>
+    <Hero/>
+    <About/>
+    <PopularAreas/>
+    <Properties/>
+    <Services/>
+    <Clients/>
+    <Contact  />
+    <Footer/>
+    </DarkModeProvider>
     </>
   )
 }
